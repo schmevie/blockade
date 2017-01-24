174a44f01efe94eee31c051e506a94e289247d6f
Frank Tran <ftransd@gmail.com>
2017-01-24 11:46:13 -0800

    package son and countdown js

--- src/js/countdown.js
+++ src/js/countdown.js
@@ -8,10 +8,10 @@ var countdown = {
         COUNTDOWN_TEXT = this.game.add.text(200, 210, COUNTDOWN_TIME_TO_START, { font: "30px Arial", fill: "#ffffff" });
     },
     decrement: function(){
-          COUNTDOWN_TIME_TO_START -= 1;
-          if(COUNTDOWN_TIME_TO_START <= 0){
-              this.game.state.start('play');
-          }
+      COUNTDOWN_TIME_TO_START -= 1;
+      if(COUNTDOWN_TIME_TO_START <= 0){
+          this.game.state.start('play');
+      }
     },
     update: function(){
         COUNTDOWN_TEXT.setText(COUNTDOWN_TIME_TO_START);
